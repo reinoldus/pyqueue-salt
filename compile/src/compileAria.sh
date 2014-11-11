ac1996aa1cd2c45ef45beb43efa5bf9242ccd514
Stefan Hesse <stefan.hesse@tu-dortmund.de>
2014-11-12 00:20:27 +0100

    fixing compileAria result false

--- compile/src/compileAria.sh
+++ compile/src/compileAria.sh
@@ -8,18 +8,18 @@ messageAndExit ()
 
 if [ -f /usr/local/bin/aria2c ]; then
 	cd /root
-	wget http://sourceforge.net/projects/aria2/files/latest/download || messageAndExit "changed=false comment='getting aria2 failed'";
-	mv download aria2.tar.bz2 || messageAndExit "changed=false comment='renaming tar failed'";
-	tar -xvjf aria2.tar.bz2 || messageAndExit "changed=false comment='extracing failed'";
+	wget http://sourceforge.net/projects/aria2/files/latest/download || messageAndExit "result=false changed=false comment='getting aria2 failed'";
+	mv download aria2.tar.bz2 || messageAndExit "result=false changed=false comment='renaming tar failed'";
+	tar -xvjf aria2.tar.bz2 || messageAndExit "result=false changed=false comment='extracing failed'";
 	rm aria2.tar.bz2
-	cd aria2-* || messageAndExit "changed=false comment='switching to aria2 dir failed'";
-	cd src || messageAndExit "changed=false comment='switching to src failed'"; 
-	patch < /root/OptionHandlerFactory.diff || messageAndExit "changed=false comment='patching failed'";
+	cd aria2-* || messageAndExit "result=false changed=false comment='switching to aria2 dir failed'";
+	cd src || messageAndExit "result=false changed=false comment='switching to src failed'"; 
+	patch < /root/OptionHandlerFactory.diff || messageAndExit "result=false changed=false comment='patching failed'";
 
 	cd ..
-	./configure --without-openssl --with-ca-bundle=/etc/ssl/certs/ca-certificates.crt --with-gnutls || messageAndExit "changed=false comment='configure failed'";
-	make -j 9 || messageAndExit "changed=false comment='make failed'";
-	make install || messageAndExit "changed=false comment='make isntall failed'";
+	./configure --without-openssl --with-ca-bundle=/etc/ssl/certs/ca-certificates.crt --with-gnutls || messageAndExit "result=false changed=false comment='configure failed'";
+	make -j 9 || messageAndExit "result=false changed=false comment='make failed'";
+	make install || messageAndExit "result=false changed=false comment='make isntall failed'";
 	
 	if [ -f /usr/local/bin/aria2c ]; then
 		messageAndExit "changed=true comment='compiled successfully'";
